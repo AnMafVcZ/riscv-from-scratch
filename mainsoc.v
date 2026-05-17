ee6ed8353e5fdb10a0a911640363da7ecfb088b7
andyz-dot <azhang4325@gmail.com>
2026-05-16 18:26:34 -0700

    Add .gitignore, update README, add mainsoc.v with clockworks

--- mainsoc.v
+++ mainsoc.v
@@ -4,6 +4,7 @@
  */
 
 `default_nettype none
+`include "clockworks.v"
 
 module SOC (
     input  CLK,        // system clock 
@@ -13,12 +14,24 @@ module SOC (
     output TXD         // UART transmit
 );
 
+   wire clk; //interal clk
+   wire resetn; //interal reset
 
 // A blinker that counts on 5 bits, wired to the 5 LEDs
    reg [4:0] count = 0;
-   always @(posedge CLK) begin
-      count <= count + 1;
+   always @(posedge clk) begin
+      count <= !resetn ? 0 : count + 1;
    end
+   
+   Clockworks#(
+      .SLOW(21) // slows it by 2^21, so it blinks at about 1Hz on a 100MHz board
+   ) clockworks (
+      .CLK(CLK),
+      .RESET(RESET),
+      .clk(clk),
+      .resetn(resetn)
+   );
+
    assign LEDS = count;
    assign TXD  = 1'b0; // not used for now
 endmodule
