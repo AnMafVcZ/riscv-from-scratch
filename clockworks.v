8a188e9cd779e2d437ac5b1f4a70e8a24eb32c75
andyz-dot <azhang4325@gmail.com>
2026-05-16 18:58:46 -0700

    Fix clockworks.v: remove placeholder dots, add default value for SLOW, wire resetn

--- clockworks.v
+++ clockworks.v
@@ -2,15 +2,16 @@ module Clockworks
 (
    input  CLK,   // clock pin of the board
    input  RESET, // reset pin of the board
-   output clk,   // (optionally divided) clock for the design.
-   output resetn // (optionally timed) negative reset for the design (more on this later)
+   output clk,   // (optionally divided) clock for the design
+   output resetn // negative reset for the design
 );
-   parameter SLOW;
-...
+   parameter SLOW = 0;
+
    reg [SLOW:0] slow_CLK = 0;
    always @(posedge CLK) begin
       slow_CLK <= slow_CLK + 1;
    end
-   assign clk = slow_CLK[SLOW];
-...
+   assign clk    = slow_CLK[SLOW];
+   assign resetn = !RESET;
+
 endmodule
